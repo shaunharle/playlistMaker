5654cd4624c9b5e84c69987024e8a305c5dc4a8d
Shaun Harle <shaunharle@Shauns-MacBook-Pro.local>
2019-11-08 11:29:17 -0500

    updating code for heroku

--- models/music.js
+++ models/music.js
@@ -1,15 +1,35 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+//___________________
+//Dependencies
+//___________________
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const musicSchema = Schema({
-    bandname: {
+//___________________
+//Set up Schema
+//___________________
+const recordSchema = new Schema({
+    name: {
         type: String,
-        required: true
+        required: [true, 'No one will buy it if it does not have a name']
     },
-    albumName: { type: String, required: true },
-    trackName: [String]
-})
+    description: String,
+    img: String,
+    price: {
+        type: Number,
+        min: [0, 'Price can\'t be less than 0. This ain\'t no charity!']
+    },
+    qty: {
+        type: Number,
+        min: [0, 'Quantity can\'t be less than 0. No capatalist joyrides here!']
+    }
+});
 
-const Music = mongoose.model('Music', musicSchema)
+//___________________
+//Set up Model
+//___________________
+const Records = mongoose.model('Records', recordSchema);
 
-module.exports = Music
+//___________________
+////Module Exports - access Records in controllers/music.js
+//___________________
+module.exports = Records;
--- server.js
+++ server.js
@@ -1,40 +1,45 @@
+
 // DEPENDENCIES
 const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const app = express()
-//PORT
+// PORT
 const PORT = process.env.PORT || 3000
 // DATABASE
-mongoURI = process.env.MONGOURI || 'mongodb://localhost/playlistMaker'
+mongoURI = process.env.MONGOURI || 'mongodb://localhost/records'
 // CONNECT TO DATABASE
 mongoose.connect(mongoURI)
 // SET CONNECTION TO CONST VAR
 const db = mongoose.connection
-// ERROR MESSAGE HANDLING
-db.on('error', console.error.bind(console, 'connection error: '))
+// DON'T KNOW WHAT THIS IS. ERROR MESSAGE STUFF OR SOMETHING
+// FOUND IT IN MONDAY'S MORNING EXERCISE
+db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function () {
-    console.log('DB: Connected')
-})
+    console.log('DB: Connected');
+});
 // CONTROLLERS
-const musicController = require('./controllers/music.js')
+// const Music = require('./models/music.js')
+const musicController = require('./controllers/music.js');
 // MIDDLEWARE
-app.use(express.static(__dirname + 'public'))
+app.use(express.static('public'))
+// app.use(express.static(__dirname+'public'))
+// this will ensure correct linking to the public folder no matter what
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/music', musicController)
 
 mongoose.connection.once('open', () => {
-    console.log('telegram for mongo')
-})
-
-// GET 
+    console.log('telegram for mongo');
+});
+// GET
 app.get('/', (req, res) => {
     res.redirect('/music')
     console.log('app is running')
 })
 
-// LISTEN
-app.listen(PORT, () => console.log('ALLOW US TO COLLECT DATE FOR TARGETED ADS'))
+
+app.listen(PORT, () => console.log('ALLOW US TO COLLECT DATA FOR TARGETED ADS. '))
+
